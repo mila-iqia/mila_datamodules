1984ade2836552b4c44333c7cda81eff063d9c2d
Fabrice Normandin <fabrice.normandin@gmail.com>
2022-08-26 18:12:56 -0400

    Trying to make a SearchPathPlugin for Hydra
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- setup.py
+++ setup.py
@@ -6,12 +6,13 @@ setup(
     description="DataModules adapted for the Mila cluster.",
     author="Fabrice Normandin",
     author_email="fabrice.normandin@gmail.com",
-    packages=find_packages(include=["mila_datamodules.*"]),
+    packages=find_packages(include=["mila_datamodules.*"])
+    + find_namespace_packages(include=["hydra_plugins.*"]),
     python_requires=">=3.7",
     # External packages as dependencies
     install_requires=[
-        "pytorch-lightning==1.6.0",
-        "lightning-bolts==0.5.0",
+        "pytorch-lightning",
+        "lightning-bolts",
     ],
     extras_require={"ffcv": "ffcv"},
 )
