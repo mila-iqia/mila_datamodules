288808c86af2b0bd4928bad7daa8a25a5c4b0e66
Fabrice Normandin <fabrice.normandin@gmail.com>
2022-09-26 18:40:15 -0400

    Move constants block in registry.py to top
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- mila_datamodules/registry.py
+++ mila_datamodules/registry.py
@@ -41,6 +41,48 @@ too_large_for_slurm_tmpdir: set[Callable] = set()
 """ Set of datasets which are too large to store in $SLURM_TMPDIR."""
 
 
+dataset_files = {
+    tvd.MNIST: ["MNIST"],
+    tvd.CIFAR10: ["cifar-10-batches-py"],
+    tvd.CIFAR100: ["cifar-100-python"],
+    tvd.FashionMNIST: ["FashionMNIST"],
+    tvd.Caltech101: ["caltech101"],
+    tvd.Caltech256: ["caltech256"],
+    tvd.CelebA: ["celeba"],
+    tvd.Cityscapes: ["leftImg8bit", "gtFine", "gtCoarse"],
+    tvd.INaturalist: ["2021_train", "2021_train_mini", "2021_valid"],
+    tvd.Places365: [
+        "categories_places365.txt",
+        "places365_train_standard.txt",
+        "data_large_standard",
+        "places365_test.txt",
+        "places365_val.txt",
+        "val_large",
+    ],
+    tvd.STL10: ["stl10_binary"],
+    tvd.SVHN: ["train_32x32.mat", "test_32x32.mat", "extra_32x32.mat"],
+    # NOTE: We're not currently using the `test` folder for the coco datasets, since there isn't
+    # an annotation file with it. (this makes sense, you probably have to submit predictions to the
+    # website or something).
+    tvd.CocoDetection: ["annotations", "test2017", "train2017", "val2017"],
+    tvd.CocoCaptions: ["annotations", "test2017", "train2017", "val2017"],
+    tvd.EMNIST: ["EMNIST"],
+    pl_bolts.datasets.BinaryMNIST: ["MNIST"],
+    pl_bolts.datasets.BinaryEMNIST: ["EMNIST"],
+}
+"""A map of the folder/files associated with each dataset type, relative to the `root_dir`. This is
+roughly the list of files/folders that would be downloaded when creating the dataset with
+`download=True`.
+
+NOTE: An entry being in this dict *does not* mean that this dataset is available on the cluster!
+This is simply a list of the files that are expected to be present in the `root` directory of each
+dataset type in order for it to work.
+
+These files are copied over to $SCRATCH or $SLURM_TMPDIR before the dataset is read, depending on
+the type of dataset.
+"""
+
+
 def is_stored_on_cluster(dataset_cls: type, cluster: Cluster | None = Cluster.current()) -> bool:
     """Returns whether we know where to find the given dataset on the given cluster."""
     return dataset_cls in dataset_roots_per_cluster and bool(
@@ -117,45 +159,3 @@ def get_dataset_root(
             f"please make an issue at {github_issue_url} so the registry can be updated."
         )
     return str(dataset_root)
-
-
-dataset_files = {
-    tvd.MNIST: ["MNIST"],
-    tvd.CIFAR10: ["cifar-10-batches-py"],
-    tvd.CIFAR100: ["cifar-100-python"],
-    tvd.FashionMNIST: ["FashionMNIST"],
-    tvd.Caltech101: ["caltech101"],
-    tvd.Caltech256: ["caltech256"],
-    tvd.CelebA: ["celeba"],
-    tvd.Cityscapes: ["leftImg8bit", "gtFine", "gtCoarse"],
-    tvd.INaturalist: ["2021_train", "2021_train_mini", "2021_valid"],
-    tvd.Places365: [
-        "categories_places365.txt",
-        "places365_train_standard.txt",
-        "data_large_standard",
-        "places365_test.txt",
-        "places365_val.txt",
-        "val_large",
-    ],
-    tvd.STL10: ["stl10_binary"],
-    tvd.SVHN: ["train_32x32.mat", "test_32x32.mat", "extra_32x32.mat"],
-    # NOTE: We're not currently using the `test` folder for the coco datasets, since there isn't
-    # an annotation file with it. (this makes sense, you probably have to submit predictions to the
-    # website or something).
-    tvd.CocoDetection: ["annotations", "test2017", "train2017", "val2017"],
-    tvd.CocoCaptions: ["annotations", "test2017", "train2017", "val2017"],
-    tvd.EMNIST: ["EMNIST"],
-    pl_bolts.datasets.BinaryMNIST: ["MNIST"],
-    pl_bolts.datasets.BinaryEMNIST: ["EMNIST"],
-}
-"""A map of the folder/files associated with each dataset type, relative to the `root_dir`. This is
-roughly the list of files/folders that would be downloaded when creating the dataset with
-`download=True`.
-
-NOTE: An entry being in this dict *does not* mean that this dataset is available on the cluster!
-This is simply a list of the files that are expected to be present in the `root` directory of each
-dataset type in order for it to work.
-
-These files are copied over to $SCRATCH or $SLURM_TMPDIR before the dataset is read, depending on
-the type of dataset.
-"""
