1f42276e331cbed7b21340cfb030abd3faaeedf2
Fabrice Normandin <fabrice.normandin@gmail.com>
2022-09-27 17:49:00 -0400

    Add TODO for Caltech256
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- mila_datamodules/vision/datasets/caltech.py
+++ mila_datamodules/vision/datasets/caltech.py
@@ -5,6 +5,7 @@ from pathlib import Path
 from typing import Callable
 
 from torchvision.datasets import Caltech101 as _Caltech101
+from torchvision.datasets import Caltech256 as _Caltech256
 
 
 class Caltech101(_Caltech101):
@@ -29,3 +30,21 @@ class Caltech101(_Caltech101):
         if not background_google_folder.exists():
             background_google_folder.mkdir(parents=True, exist_ok=False)
         super().__init__(root, target_type, transform, target_transform, download)
+
+
+# TODO: This dataset doesn't work: Indexing into the dataset looks for an unexisting file
+# (014.blimp/001_0001.jpg).
+# I redownloaded it into SCRATCH, and it still has the exact same structure, and raises the same
+# error!
+
+
+class Caltech256(_Caltech256):
+    # TODO: Apply some kind of patch to fix the problem with this dataset.
+    def __init__(
+        self,
+        root: str,
+        transform: Callable | None = None,
+        target_transform: Callable | None = None,
+        download: bool = False,
+    ) -> None:
+        super().__init__(root, transform, target_transform, download)
