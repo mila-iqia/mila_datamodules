86a1a4b019d6b293563f63ec1a7e808f00db731b
Fabrice Normandin <fabrice.normandin@gmail.com>
2022-10-03 18:16:43 -0400

    Fix fixture parallelism issues
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- mila_datamodules/conftest.py
+++ mila_datamodules/conftest.py
@@ -2,6 +2,7 @@ import os
 import shutil
 
 import pytest
+from filelock import FileLock
 
 from mila_datamodules.clusters import SLURM_TMPDIR
 
@@ -14,10 +15,11 @@ def clear_slurm_tmpdir():
     weird errors because the datasets were half-moved to SLURM_TMPDIR, so reading them on the next
     run would fail.
     """
-    if (SLURM_TMPDIR / "data").exists():
-        # NOTE: The `chmod_recursive` doesn't seem to work atm. It's safe to assume we're on Linux,
-        # so this is fine for now.
-        # chmod_recursive(SLURM_TMPDIR / "data", 0o644)
-        os.system(f"chmod --recursive +rwx {SLURM_TMPDIR}/data")
-        shutil.rmtree(SLURM_TMPDIR / "data")
+    with FileLock(SLURM_TMPDIR / "data.lock"):
+        if (SLURM_TMPDIR / "data").exists():
+            # NOTE: The `chmod_recursive` doesn't seem to work atm. It's safe to assume we're on Linux,
+            # so this is fine for now.
+            # chmod_recursive(SLURM_TMPDIR / "data", 0o644)
+            os.system(f"chmod --recursive +rwx {SLURM_TMPDIR}/data")
+            shutil.rmtree(SLURM_TMPDIR / "data")
     yield
