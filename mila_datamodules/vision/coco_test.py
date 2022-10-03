338e9eb936f22dbc8ddb74a91ed755e0e2c4c7d6
Fabrice Normandin <fabrice.normandin@gmail.com>
2022-10-03 18:02:28 -0400

    Increase timeout for COCO tests
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- mila_datamodules/vision/coco_test.py
+++ mila_datamodules/vision/coco_test.py
@@ -1,6 +1,7 @@
 import itertools
 from typing import ClassVar
 
+import pytest
 from torch import Tensor
 
 from .coco import CocoCaptionsDataModule
@@ -8,6 +9,7 @@ from .vision_datamodule import VisionDataModule
 from .vision_datamodule_test import VisionDataModuleTests
 
 
+@pytest.mark.timeout(300)
 class TestCoco(VisionDataModuleTests):
     DataModule: ClassVar[type[VisionDataModule]] = CocoCaptionsDataModule
 
@@ -20,6 +22,7 @@ class TestCoco(VisionDataModuleTests):
         for x, y in itertools.islice(train_dataloader, 2):
             assert isinstance(x, Tensor)
             # TODO: Should we make this y 'cleaner', e.g. by making it into a list[str] ?
+            y: list[tuple[str]]
             assert isinstance(y, list)
             assert all(isinstance(y_i, tuple) for y_i in y)
             assert all(isinstance(y_i_j, str) for y_i in y for y_i_j in y_i)
