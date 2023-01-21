c9b2d0110b6a26ee28b9b5fe58f7dda1dffdba07
Fabrice Normandin <fabrice.normandin@gmail.com>
2023-01-20 22:25:59 -0500

    Add scripts to run tests on mila, beluga clusters
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

---
+++ run_all_tests.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/bash
+
+# IDEA: Runs all the tests of each cluster.
+
+# BRANCH=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
+
+function copy_repo_folder_to {
+    # Adapted from https://stackoverflow.com/questions/13713101/rsync-exclude-according-to-gitignore-hgignore-svnignore-like-filter-c
+    local cluster=$1
+    local src="."
+    local dest="mila_datamodules"
+    # Other interesting solution, using git ls-files:
+    # BUG: Seems to make the program hang?
+    # rsync -ah --delete --verbose --include .git \
+    #     --exclude-from=<(git -C $src ls-files --exclude-standard -oi --directory) \
+    #     $src $cluster:mila_datamodules
+
+    rsync -h --verbose --recursive -a $src $cluster:$dest \
+        --include='**.gitignore' --exclude='/.git' --filter=':- .gitignore' --delete-after
+}
+
+
+function run_in_mila {
+    copy_repo_folder_to mila
+    ssh mila 'bash -s' < scripts/mila_test.sh
+}
+
+
+# For Beluga / CC, there are steps that need to be done before running the job, since it doesn't
+# have internet access on the compute nodes (clone / pull the code from GitHub, activate the
+# virtualenv, install the packages, etc.
+
+
+function run_in_beluga {
+    copy_repo_folder_to beluga
+    ssh beluga 'bash -s' < scripts/beluga_test.sh
+}
+
+
+run_in_mila
+# run_in_beluga
+# wait
+# run_on_mila_cluster
+# run_on_beluga
