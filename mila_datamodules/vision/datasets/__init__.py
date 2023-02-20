70fd4353693d033bab62e35f485facfaaa8e95c3
Fabrice Normandin <fabrice.normandin@gmail.com>
2023-02-20 14:00:48 -0500

    Remove Caltech101 adapter (no longer needed)
    - Removed the Caltech101 adapter, since the bug was fixed in
    torchvision, and therefore the adapter isn't needed anymore
    
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- mila_datamodules/vision/datasets/__init__.py
+++ mila_datamodules/vision/datasets/__init__.py
@@ -6,7 +6,11 @@ on the current cluster.
 They might also silently first copy the dataset to SLURM_TMPDIR before reading it, depending on the
 size of the dataset.
 
-NOTE: These wrappers don't do anything when this is ran outside a SLURM cluster.
+NOTE: These wrappers don't do anything when outside a SLURM cluster.
+
+TODO: For some of these datasets that have other required arguments, perhaps we could add "good"
+default values for some of them? (Currently doing that in the VisionDataModule associated with
+this dataset).
 """
 from __future__ import annotations
 
@@ -14,35 +18,29 @@ import torchvision.datasets
 
 from .adapted_datasets import adapt_dataset
 from .binary_mnist import BinaryEMNIST, BinaryMNIST
-from .caltech101 import Caltech101
 from .imagenet import ImageNet
 from .mnist import MNIST
 
 # NOTE: These here use "Patched" versions of the datasets.
 MNIST = adapt_dataset(MNIST)
-CIFAR10 = adapt_dataset(torchvision.datasets.CIFAR10)
-CIFAR100 = adapt_dataset(torchvision.datasets.CIFAR100)
-FashionMNIST = adapt_dataset(torchvision.datasets.FashionMNIST)
-
-Caltech101 = adapt_dataset(Caltech101)
-# Caltech101 = adapt_dataset(torchvision.datasets.Caltech101)
 ImageNet = adapt_dataset(ImageNet)
 
+Caltech101 = adapt_dataset(torchvision.datasets.Caltech101)
 Caltech256 = adapt_dataset(torchvision.datasets.Caltech256)
 CelebA = adapt_dataset(torchvision.datasets.CelebA)
 Cityscapes = adapt_dataset(torchvision.datasets.Cityscapes)
+CIFAR10 = adapt_dataset(torchvision.datasets.CIFAR10)
+CIFAR100 = adapt_dataset(torchvision.datasets.CIFAR100)
+FashionMNIST = adapt_dataset(torchvision.datasets.FashionMNIST)
 INaturalist = adapt_dataset(torchvision.datasets.INaturalist)
 Places365 = adapt_dataset(torchvision.datasets.Places365)
 STL10 = adapt_dataset(torchvision.datasets.STL10)
 SVHN = adapt_dataset(torchvision.datasets.SVHN)
-# TODO: For some of these datasets that have other required arguments, perhaps we could add "good"
-# default values for some of them?
-# e.g. EMNIST: split="mnist"
-#     CocoCaptions: annFile=/wherever_its_stored/annotations/captions_train2017.json
+
+EMNIST = adapt_dataset(torchvision.datasets.EMNIST)
 CocoDetection = adapt_dataset(torchvision.datasets.CocoDetection)
 CocoCaptions = adapt_dataset(torchvision.datasets.CocoCaptions)
 
-EMNIST = adapt_dataset(torchvision.datasets.EMNIST)
 
 BinaryMNIST = adapt_dataset(BinaryMNIST)
 BinaryEMNIST = adapt_dataset(BinaryEMNIST)
