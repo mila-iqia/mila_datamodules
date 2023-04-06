e8e613e83763f821f1ab73348fb5938c4c10fef1
Fabrice Normandin <normandf@mila.quebec>
2023-04-06 18:42:54 -0400

    Add ImageNet option for the Mila cluster
    Signed-off-by: Fabrice Normandin <normandf@mila.quebec>

--- mila_datamodules/cli/prepare.py
+++ mila_datamodules/cli/prepare.py
@@ -6,11 +6,12 @@ import shutil
 from typing import Callable, Generic, Sequence, TypeVar
 from typing_extensions import ParamSpec, Concatenate
 import inspect
+from simple_parsing import field
 
 from mila_datamodules.clusters.cluster import Cluster
 import torchvision.datasets as tvd
 
-from .utils import runs_on_local_main_process_first, is_local_main
+from mila_datamodules.cli.utils import runs_on_local_main_process_first, is_local_main
 
 # from simple_parsing import ArgumentParser
 SLURM_TMPDIR = Path(os.environ["SLURM_TMPDIR"])
@@ -25,14 +26,31 @@ class PrepareVisionDataset(Generic[VD, P]):
     def __init__(self, dataset_type: Callable[Concatenate[str, P], VD]):
         self.dataset_type = dataset_type
 
-    @runs_on_local_main_process_first
     def __call__(
         self,
         root: str | Path = SLURM_TMPDIR / "datasets",
         *constructor_args: P.args,
         **constructor_kwargs: P.kwargs,
     ) -> str:
+        """Use the dataset constructor to prepare the dataset in the `root` directory.
+
+        If the dataset has a `download` argument in its constructor, it will be set to `True` so
+        the archives are extracted.
+
+        NOTE: This should only really be called after the actual dataset preparation has been done
+        in a subclass's `__call__` method.
+
+        Returns `root` (as a string).
+        """
         Path(root).mkdir(parents=True, exist_ok=True)
+
+        constructor_kwargs = constructor_kwargs.copy()  # type: ignore
+        if "download" in inspect.signature(self.dataset_type).parameters:
+            constructor_kwargs["download"] = True
+
+        print(
+            f"Using dataset constructor: {self.dataset_type} with args {constructor_args}, and kwargs {constructor_kwargs}"
+        )
         dataset_instance = self.dataset_type(str(root), *constructor_args, **constructor_kwargs)
         if is_local_main():
             print(dataset_instance)
@@ -40,13 +58,29 @@ class PrepareVisionDataset(Generic[VD, P]):
 
 
 class SymlinkArchives(PrepareVisionDataset[VD, P]):
+    """Creates symlinks to the archives in the `root` directory."""
+
     def __init__(
         self,
         dataset_type: Callable[Concatenate[str, P], VD],
-        relative_paths_to_archives: dict[str, Path],
+        relative_paths_to_archives: dict[str, str | Path],
     ):
+        """
+
+        Parameters
+        ----------
+
+        - dataset_type:
+            Callable that returns an instance of a `torchvision.datasets.VisionDataset`.
+
+        - relative_paths_to_archives:
+            A mapping from a relative path where the symlink to the archive should be created
+            (relative to the 'root' directory) to the actual path to the archive on the cluster.
+        """
         self.dataset_type = dataset_type
-        self.relative_paths_to_archives = relative_paths_to_archives
+        self.relative_paths_to_archives = {
+            k: Path(v) for k, v in relative_paths_to_archives.items()
+        }
 
     @runs_on_local_main_process_first
     def __call__(
@@ -68,21 +102,13 @@ class SymlinkArchives(PrepareVisionDataset[VD, P]):
             archive_symlink.parent.mkdir(parents=True, exist_ok=True)
             archive_symlink.symlink_to(archive)
             print(f"Making link from {archive_symlink} -> {archive}")
-
-        # Use the dataset constructor to extract the archives.
-        # Note: need to use `download=True` for torchvision to extract the archive.
-        constructor_kwargs = constructor_kwargs.copy()  # type: ignore
-        if "download" in inspect.signature(self.dataset_type).parameters:
-            constructor_kwargs["download"] = True
-
-        _dataset_instance = self.dataset_type(str(root), *constructor_args, **constructor_kwargs)
-        if is_local_main():
-            print(_dataset_instance)
-
-        return str(root)
+        # TODO: Check that nesting `runs_on_local_main_process_first` decorators isn't a problem.
+        return super().__call__(root, *constructor_args, **constructor_kwargs)
 
 
 class CopyTree(PrepareVisionDataset[VD, P]):
+    """Copies a tree of files from the cluster to the `root` directory."""
+
     def __init__(
         self,
         dataset_type: Callable[Concatenate[str, P], VD],
@@ -115,23 +141,15 @@ class CopyTree(PrepareVisionDataset[VD, P]):
                 dirs_exist_ok=True,
             )
 
-        # Use the dataset constructor to extract the archives.
-        # Note: need to use `download=True` for torchvision to extract the archive.
-        constructor_kwargs = constructor_kwargs.copy()  # type: ignore
-        if "download" in inspect.signature(self.dataset_type).parameters:
-            constructor_kwargs["download"] = True
-
-        _dataset_instance = self.dataset_type(str(root), *constructor_args, **constructor_kwargs)
-        if is_local_main():
-            print(_dataset_instance)
-
-        return str(root)
+        return super()(root, *constructor_args, **constructor_kwargs)
 
 
-prepare_torchvision_dataset: dict[type, dict[Cluster, PrepareVisionDataset]] = {
+prepare_torchvision_datasets: dict[type, dict[Cluster, PrepareVisionDataset]] = {
     tvd.MNIST: {
-        # On the Mila cluster we have archives which are extracted into 4 "raw" binary files. Easy peasy.
-        # NOTE: On Beluga, we have the MNIST 'raw' files in /project/rpp-bengioy/data/MNIST/raw, no archives.
+        # On the Mila and Beluga cluster we have archives which are extracted into 4 "raw" binary
+        # files. We do need to match the expected directory structure of the torchvision MNIST
+        # dataset though.
+        # NOTE: On Beluga, we also have the MNIST 'raw' files in /project/rpp-bengioy/data/MNIST/raw, no archives.
         Cluster.Mila: SymlinkArchives(
             tvd.MNIST,
             {
@@ -139,106 +157,55 @@ prepare_torchvision_dataset: dict[type, dict[Cluster, PrepareVisionDataset]] = {
                 for archive in Path("/network/datasets/mnist").glob("*.gz")
             },
         ),
-        Cluster.Beluga: CopyTree(tvd.MNIST, {"MNIST": "/project/rpp-bengioy/data/MNIST"}),
+        Cluster.Beluga: SymlinkArchives(
+            tvd.MNIST,
+            {
+                f"MNIST/raw/{archive.name}": archive
+                for archive in Path("/project/rpp-bengioy/data/curated/mnist").glob("*.gz")
+            },
+        ),
     },
     tvd.CIFAR10: {
         Cluster.Mila: SymlinkArchives(
             tvd.CIFAR10,
-            {"cifar-10-python.tar.gz": Path("/network/datasets/cifar10/cifar-10-python.tar.gz")},
+            {"cifar-10-python.tar.gz": "/network/datasets/cifar10/cifar-10-python.tar.gz"},
         ),
         Cluster.Beluga: SymlinkArchives(
             tvd.CIFAR10,
             {
-                "cifar-10-python.tar.gz": Path(
-                    "/project/rpp-bengioy/data/curated/cifar10/cifar-10-python.tar.gz"
-                ),
+                "cifar-10-python.tar.gz": "/project/rpp-bengioy/data/curated/cifar10/cifar-10-python.tar.gz",
+            },
+        ),
+    },
+    tvd.CIFAR100: {
+        Cluster.Mila: SymlinkArchives(
+            tvd.CIFAR100,
+            {"cifar-100-python.tar.gz": "/network/datasets/cifar100/cifar-100-python.tar.gz"},
+        ),
+        Cluster.Beluga: SymlinkArchives(
+            tvd.CIFAR100,
+            {
+                "cifar-100-python.tar.gz": "/project/rpp-bengioy/data/curated/cifar100/cifar-100-python.tar.gz",
+            },
+        ),
+    },
+    tvd.ImageNet: {
+        # TODO: Write a customized `PrepareVisionDataset` for ImageNet that uses Olexa's magic tar
+        # command.
+        Cluster.Mila: SymlinkArchives(
+            tvd.ImageNet,
+            {
+                "ILSVRC2012_devkit_t12.tar.gz": "/network/datasets/imagenet/ILSVRC2012_devkit_t12.tar.gz",
+                "ILSVRC2012_img_train.tar": "/network/datasets/imagenet/ILSVRC2012_img_train.tar",
+                "ILSVRC2012_img_val.tar": "/network/datasets/imagenet/ILSVRC2012_img_val.tar",
             },
         ),
     },
 }
-"""
-A mapping from the relative path where the symlink to the archive should be created (relative to
-a 'root' directory) to the actual path of the archive on the network.
-"""
+""" Dataset preparation functions per dataset type, per cluster. """
 
 # TODO: For the datasets we don't have archives for, we could either list the locations where the
 # extracted version can be found, or we could download the archive in $SCRATCH.
-
-
-# cifar10_archives = {
-#     Cluster.Mila: "/network/datasets/cifar10/cifar-10-python.tar.gz",
-#     Cluster.Beluga: "/project/rpp-bengioy/data/curated/cifar10/cifar-10-python.tar.gz",
-# }
-
-
-# def prepare_cifar10(
-#     dataset: type[tvd.CIFAR10] = tvd.CIFAR10,
-#     root: Path = SLURM_TMPDIR / "datasets",
-# ) -> Path:
-#     cluster = Cluster.current_or_error()
-#     archive = Path(cifar10_archives[cluster])
-#     assert archive.exists()
-
-#     with local_main_process_first():
-#         if is_local_main():
-#             root.mkdir(parents=True, exist_ok=True)
-
-#             archive_name = archive.name
-#             # Make a symlink in the local scratch directory to the archive on the network.
-#             (Path(root) / archive_name).symlink_to(archive)
-#             print(f"Making link from {root / archive_name} -> {archive}")
-
-#         # Note: need to use `download=True` for torchvision to extract the archive.
-#         dataset(str(root), download=True)
-
-#     return root
-
-
-# def prepare_mnist_beluga(
-#     dataset: type[tvd.MNIST] = tvd.MNIST, root: Path = SLURM_TMPDIR / "datasets"
-# ) -> Path:
-#     shutil.copytree("/project/rpp-bengioy/data/MNIST", root / "MNIST")
-#     return root
-
-
-# def prepare_mnist_mila(
-#     dataset: type[tvd.MNIST] = tvd.MNIST,
-#     root: Path = SLURM_TMPDIR / "datasets",
-# ) -> Path:
-#     relative_path_to_archive = {
-#         f"MNIST/raw/{archive.name}": archive
-#         for archive in Path("/network/datasets/mnist").glob("*.gz")
-#     }
-#     with local_main_process_first():
-#         root.mkdir(parents=True, exist_ok=True)
-
-#         for relative_path, archive in relative_path_to_archive.items():
-#             assert archive.exists()
-#             # Make a symlink in the local scratch directory to the archive on the network.
-#             archive_symlink = root / relative_path
-#             if archive_symlink.exists():
-#                 continue
-
-#             archive_symlink.parent.mkdir(parents=True, exist_ok=True)
-#             archive_symlink.symlink_to(archive)
-#             print(f"Making link from {archive_symlink} -> {archive}")
-
-#         # Note: need to use `download=True` for torchvision to extract the archive.
-#         _ = dataset(str(root), download=True)
-
-#     return root
-
-
-# prepare_dataset_functions = {
-#     tvd.MNIST: {
-#         Cluster.Mila: prepare_mnist_mila,
-#         Cluster.Beluga: prepare_mnist_beluga,
-#     },
-#     tvd.CIFAR10: {
-#         Cluster.Mila: prepare_cifar10,
-#         Cluster.Beluga: prepare_cifar10,
-#     },
-# }
 
 
 def prepare(argv: list[str] | None = None):
@@ -248,13 +215,21 @@ def prepare(argv: list[str] | None = None):
         title="dataset", description="Which dataset to prepare", dest="dataset"
     )
 
-    cifar10_parser = subparsers.add_parser("cifar10", help="Prepare the CIFAR10 dataset")
-    cifar10_parser.add_argument("--root", type=Path, default=SLURM_TMPDIR / "datasets")
-    cifar10_parser.set_defaults(function=prepare_torchvision_dataset[tvd.CIFAR10][current_cluster])
+    dataset_preparation_functions = {
+        dataset_type: prepare_dataset_fns[current_cluster]
+        for dataset_type, prepare_dataset_fns in prepare_torchvision_datasets.items()
+        if current_cluster in prepare_dataset_fns
+    }
 
-    mnist_parser = subparsers.add_parser("mnist", help="Prepare the mnist dataset")
-    mnist_parser.add_argument("--root", type=Path, default=SLURM_TMPDIR / "datasets")
-    mnist_parser.set_defaults(function=prepare_torchvision_dataset[tvd.MNIST][current_cluster])
+    for dataset_type, prepare_dataset_fn in dataset_preparation_functions.items():
+        dataset_parser = subparsers.add_parser(
+            dataset_type.__name__.lower(), help=f"Prepare the {dataset_type.__name__} dataset"
+        )
+        dataset_parser.add_argument("--root", type=Path, default=SLURM_TMPDIR / "datasets")
+        # IDEA: Add a way for the dataset preparation thingy to add its own arguments
+        # (e.g. --split='train'/'val')
+        # prepare_dataset_fn.add_arguments(dataset_parser)
+        dataset_parser.set_defaults(function=prepare_dataset_fn)
 
     args = parser.parse_args(argv)
 
