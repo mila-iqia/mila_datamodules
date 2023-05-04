68a2c1a35fb2dff515d49dfb88d8a56038791dc0
Fabrice Normandin <normandf@mila.quebec>
2023-05-04 13:20:00 -0400

    Split the torchvision and huggingface prepare fns
    Signed-off-by: Fabrice Normandin <normandf@mila.quebec>

--- mila_datamodules/cli/prepare.py
+++ mila_datamodules/cli/prepare.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import argparse
 import logging
 from dataclasses import asdict
+from logging import getLogger as get_logger
 from pathlib import Path
 from typing import Callable
 
@@ -34,6 +35,7 @@ from mila_datamodules.cli.torchvision import (
 from mila_datamodules.clusters.cluster import Cluster
 
 current_cluster = Cluster.current_or_error()
+logger = get_logger(__name__)
 
 
 def add_prepare_arguments(parser: ArgumentParser):
@@ -80,6 +82,8 @@ def add_torchvision_prepare_args(
         # prepare_dataset_fn.add_arguments(dataset_parser)
         dataset_parser.set_defaults(function=prepare_dataset_fn)
 
+        dataset_parser.set_defaults(prepare_fn=prepare_torchvision)
+
 
 def add_huggingface_prepare_arguments(
     subparsers: argparse._SubParsersAction[simple_parsing.ArgumentParser],
@@ -98,6 +102,8 @@ def add_huggingface_prepare_arguments(
         command_line_args = command_line_args_for_hf_dataset[dataset_name]
         dataset_parser.add_arguments(command_line_args, dest="dataset_preparation")
         dataset_parser.set_defaults(function=prepare_hf_dataset_fn)
+
+        dataset_parser.set_defaults(prepare_fn=prepare_huggingface)
 
 
 def prepare(args):
@@ -127,16 +133,13 @@ def prepare(args):
     hf_logger.setLevel(_level(verbose - 2))
     hf_logger.addHandler(rich.logging.RichHandler(markup=True, tracebacks_width=100))
 
-    # logging.basicConfig(
-    #     level=_level(verbose),
-    #     format=f"[node{get_node_index()}-task{get_local_rank()}] %(name)s - %(message)s ",
-    #     handlers=[rich.logging.RichHandler(markup=True, tracebacks_width=100)],
-    # )
-
-    # logger.debug(args)
     # TODO: Dispatch what to do with `args` (and the output of the function) in a smarter way,
     # based on which module was selected.
+    prepare_fn: Callable[[dict], None] = args_dict.pop("prepare_fn")
+    prepare_fn(args_dict)
 
+
+def prepare_torchvision(args_dict: dict):
     dataset: str = args_dict.pop("dataset")
     function: Callable = args_dict.pop("function")
     dataset_arguments = args_dict.pop("dataset_preparation", None)
@@ -153,52 +156,68 @@ def prepare(args):
 
     logger.setLevel(logging.INFO)
 
-    if isinstance(output, (str, Path)):
-        new_root = output
-        # TODO: For some datasets (e.g. Coco), it's not actually true! We would like to tell users
-        # how exactly the dataset can be created, for instance:
-        # ```
-        # tvd.CocoCaptions(
-        #     root=f"{os.environ['SLURM_TMPDIR']}/datasets/train2017",
-        #     annFile=f"{os.environ['SLURM_TMPDIR']}/datasets/annotations/stuff_train2017.json"
-        # )
-        # ```
-        import torchvision.datasets
+    new_root = output
+    # TODO: For some datasets (e.g. Coco), it's not actually true! We would like to tell users
+    # how exactly the dataset can be created, for instance:
+    # ```
+    # tvd.CocoCaptions(
+    #     root=f"{os.environ['SLURM_TMPDIR']}/datasets/train2017",
+    #     annFile=f"{os.environ['SLURM_TMPDIR']}/datasets/annotations/stuff_train2017.json"
+    # )
+    # ```
+    import torchvision.datasets
 
-        # FIXME: Ugly AF, just there for the demo:
-        dataset_class = {
-            k: v for k, v in vars(torchvision.datasets).items() if k.lower() == dataset
-        }.popitem()[1]
-        # if isinstance(dataset_arguments, CocoDetectionArgs):
-        #     code_snippet = dataset_arguments.code_to_use()
-        # else:
-        kwargs.update(root=new_root)
-        code_snippet = (
-            f"{dataset_class.__name__}(" + ", ".join(f"{k}={v!r}" for k, v in kwargs.items()) + ")"
-        )
-        slurm_tmpdir = get_slurm_tmpdir()
-        code_snippet = (
-            (code_snippet)
-            .replace(f'"{slurm_tmpdir}', 'os.environ["SLURM_TMPDIR"] + "')
-            .replace(f"'{slurm_tmpdir}", "os.environ['SLURM_TMPDIR'] + '")
-        )
-        # fn = logger.info if verbose > 0 else print
-        logger.info(
-            "Here's how you can use this prepared dataset in your job:\n"
-            + "\n"
-            + "```python\n"
-            + "import os\n"
-            + code_snippet
-            + "\n```"
-        )
-    else:
-        assert isinstance(output, HfDatasetsEnvVariables)
-        logger.info(
-            "The following environment variables have been set in this process, but will "
-            "probably also need to also be added in the job script:"
-        )
-        for key, value in asdict(output).items():
-            print(f"export {key}={value}")
+    # FIXME: Ugly AF, just there for the demo:
+    dataset_class = {
+        k: v for k, v in vars(torchvision.datasets).items() if k.lower() == dataset
+    }.popitem()[1]
+    # if isinstance(dataset_arguments, CocoDetectionArgs):
+    #     code_snippet = dataset_arguments.code_to_use()
+    # else:
+    kwargs.update(root=new_root)
+    code_snippet = (
+        f"{dataset_class.__name__}(" + ", ".join(f"{k}={v!r}" for k, v in kwargs.items()) + ")"
+    )
+    slurm_tmpdir = get_slurm_tmpdir()
+    code_snippet = (
+        (code_snippet)
+        .replace(f'"{slurm_tmpdir}', 'os.environ["SLURM_TMPDIR"] + "')
+        .replace(f"'{slurm_tmpdir}", "os.environ['SLURM_TMPDIR'] + '")
+    )
+    # fn = logger.info if verbose > 0 else print
+    logger.info(
+        "Here's how you can use this prepared dataset in your job:\n"
+        + "\n"
+        + "```python\n"
+        + "import os\n"
+        + code_snippet
+        + "\n```"
+    )
+
+
+def prepare_huggingface(args_dict: dict):
+    function: Callable = args_dict.pop("function")
+    dataset_arguments = args_dict.pop("dataset_preparation", DatasetArguments())
+
+    kwargs = args_dict
+
+    if dataset_arguments:
+        assert isinstance(dataset_arguments, DatasetArguments)
+        additional_kwargs = dataset_arguments.to_dataset_kwargs()
+        assert not any(k in kwargs for k in additional_kwargs)
+        kwargs.update(additional_kwargs)
+
+    output = function(**kwargs)
+
+    logger.setLevel(logging.INFO)
+
+    assert isinstance(output, HfDatasetsEnvVariables)
+    logger.info(
+        "The following environment variables have been set in this process, but will "
+        "probably also need to also be added in the job script:"
+    )
+    for key, value in asdict(output).items():
+        print(f"export {key}={value}")
 
 
 def get_env_variables_to_use():
