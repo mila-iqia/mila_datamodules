506ca06f37da94ca3e466fbe0a58b1868af40de4
Fabrice Normandin <normandf@mila.quebec>
2023-05-08 16:53:32 -0400

    Update the populate_shared_cache script
    Signed-off-by: Fabrice Normandin <normandf@mila.quebec>

--- mila_datamodules/cli/shared_cache/populate_shared_cache.py
+++ mila_datamodules/cli/shared_cache/populate_shared_cache.py
@@ -1,3 +1,4 @@
+#!/home/mila/n/normandf/.conda/envs/datamodules/bin/python
 """Downloads stuff from HuggingFace/torchvision/etc that many researchers might want to use.
 
 If you want to add an entry to this file, please send a message to the IDT team. (or specifically
@@ -5,56 +6,72 @@ to Fabrice Normandin)
 """
 from __future__ import annotations
 
+import argparse
 import os
 
-SCRATCH = os.environ["SCRATCH"]
+default_shared_cache_dir = "/network/datasets/.weights/.shared_cache"
 
-shared_cache_dir = "/network/datasets/.weights/.shared_cache"
-shared_cache_dir = f"{SCRATCH}/shared_cache"
 
-os.environ["TORCH_HOME"] = f"{shared_cache_dir}/torch"
-os.environ["HF_HOME"] = f"{shared_cache_dir}/huggingface"
-os.environ.pop("TRANSFORMERS_CACHE", "")
-os.environ.pop("HUGGINGFACE_HUB_CACHE", "")
-os.environ.pop("HF_DATASETS_CACHE", "")
-
-import torch  # noqa
-from datasets import load_dataset  # noqa
-from transformers import AutoModel  # noqa
-
-models_to_download = [
-    # "bigscience/bloom",
-    # "facebook/opt-13b",
-    # "facebook/opt-30b",
-    # "facebook/opt-66b",
+hf_models_to_download = [
+    "gpt2",
+    "bigscience/bloom",
+    "facebook/opt-13b",
+    "facebook/opt-30b",
+    "facebook/opt-66b",
     "t5-large",
     "google/flan-t5-xxl",
 ]
 
-for model in models_to_download:
-    print(f"Downloading {model}")
-    # TODO: This crashes because it instantiates the whole model in memory.
-    AutoModel.from_pretrained(
-        model, resume_download=True, device_map="auto", offload_folder=f"{shared_cache_dir}/.temp"
-    )
-
-# TODO: Also download big datasets
-datasets_to_download: list[tuple[str, list[str]]] = [
+hf_datasets_to_download: list[tuple[str, list[str]]] = [
     ("c4", ["en", "realnewslike"]),
     ("gsm8k", ["main", "socratic"]),
     ("wikitext", ["wikitext-103-v1", "wikitext-2-v1", "wikitext-103-raw-v1", "wikitext-2-raw-v1"]),
 ]
-for dataset, configs in datasets_to_download:
-    for config in configs:
-        print(f"Downloading {dataset} {config}")
-        load_dataset(dataset, config, num_proc=16)
 
-
-tv_models_to_download = [
+torchvision_models_to_download = [
     "resnet18",
     "resnet50",
     "resnet101",
 ]
-for model in tv_models_to_download:
-    print(f"Downloading {model}")
-    torch.hub.load("pytorch/vision", model, weights="DEFAULT")
+
+
+def main(argv: list[str] | None = None):
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("--shared_cache_dir", type=str, default=default_shared_cache_dir)
+    args = parser.parse_args(argv)
+
+    shared_cache_dir: str = args.shared_cache_dir
+
+    os.environ["TORCH_HOME"] = f"{shared_cache_dir}/torch"
+    os.environ["HF_HOME"] = f"{shared_cache_dir}/huggingface"
+    os.environ.pop("TRANSFORMERS_CACHE", "")
+    os.environ.pop("HUGGINGFACE_HUB_CACHE", "")
+    os.environ.pop("HF_DATASETS_CACHE", "")
+
+    import torch  # noqa
+    from datasets import load_dataset  # noqa
+    from transformers import AutoModel, AutoConfig  # noqa
+
+    for model in hf_models_to_download:
+        print(f"Downloading {model}")
+
+        # TODO: This instantiates the model, which we don't need to be doing!
+        # Need to checkout how to download the model without instantiating it.
+        # from huggingface_hub import cached_download, hf_hub_download
+        # from transformers.utils.hub import cached_file
+        # NOTE: also, this saves the weights in the offload directory (which is currently in /tmp).
+        AutoModel.from_pretrained(
+            model,
+            resume_download=True,
+            device_map="auto",
+            offload_folder="/tmp/offload",
+        )
+
+    for dataset, configs in hf_datasets_to_download:
+        for config in configs:
+            print(f"Downloading {dataset} {config}")
+            load_dataset(dataset, config, num_proc=16)
+
+    for model in torchvision_models_to_download:
+        print(f"Downloading {model}")
+        torch.hub.load("pytorch/vision", model, weights="DEFAULT")
